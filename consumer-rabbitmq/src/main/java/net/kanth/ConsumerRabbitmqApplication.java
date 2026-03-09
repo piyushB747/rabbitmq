dadba42a96bb5e1a1c07bd7949bf4f0e07bd5f8e
Piyushraj Singh <singhrajpiyush@gmail.com>
2026-03-09 19:24:43 +0530

    first commit

---
+++ consumer-rabbitmq/src/main/java/net/kanth/ConsumerRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package net.kanth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerRabbitmqApplication.class, args);
+	}
+
+}
---
+++ consumer-rabbitmq/src/main/java/net/kanth/control/ControlUserCreated.java
@@ -0,0 +1,11 @@
+package net.kanth.control;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/apiuser")
+public class ControlUserCreated {
+
+
+
+}
